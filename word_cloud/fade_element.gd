440dfb1ecbedfc2740ec4bc776ed30a7af0bf8fa
Geordie - <Geordietomomcclelland@gmail.com>
2025-10-05 19:31:27 +1100

    cloud fading

--- word_cloud/fade_element.gd
+++ word_cloud/fade_element.gd
@@ -3,21 +3,35 @@ extends Control
 var fading := false
 var fade_speed := 2.0
 
+@onready var texture_rect = %TextureRect
+@onready var texture_rect_2 = %TextureRect2
+@onready var texture_rect_3 = %TextureRect3
+@onready var texture_rect_4 = %TextureRect4
+@onready var margin_container = %MarginContainer
+
 func _process(delta):
 	if fading:
-		modulate.a = clamp(modulate.a + fade_speed * delta, 0.0, 1.0)
-		if modulate.a == 1.0 or modulate.a == 0.0:
+		margin_container.modulate.a = clamp(margin_container.modulate.a + fade_speed * delta, 0.0, 1.0)
+		texture_rect.modulate.a = clamp(texture_rect.modulate.a + fade_speed * delta, 0.0, 1.0)
+		texture_rect_2.modulate.a = clamp(texture_rect_2.modulate.a + (fade_speed+0.5) * delta, 0.0, 1.0)
+		texture_rect_3.modulate.a = clamp(texture_rect_3.modulate.a + (fade_speed+1.0) * delta, 0.0, 1.0)
+		texture_rect_4.modulate.a = clamp(texture_rect_4.modulate.a + (fade_speed+1.5) * delta, 0.0, 1.0)
+		#print(fading)
+		#print(texture_rect.modulate.a)
+		if texture_rect.modulate.a == 1.0 or texture_rect_4.modulate.a == 0.0:
 			fading = false
+			print(fading)
 
 func toggle_cloud():
-	if modulate.a > 0.5:
+	if texture_rect_4.modulate.a > 0.8:
 		hide_cloud()
 	else:
 		show_cloud()
 
 func hide_cloud():
-	fading = true
-	fade_speed = -abs(fade_speed) # fade out
+	fading = true # fade out
+	fade_speed = -abs(fade_speed)
+
 
 func show_cloud():
 	fading = true
